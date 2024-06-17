5d6c29c8011f299d12caf758e2392d2ac40658ae
PatrykGodlewski <81991100+PatrykGodlewski@users.noreply.github.com>
2024-06-17 23:14:27 +0200

    added posthog analitics

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,20 +1,29 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 
 interface Props {
   readonly children: React.ReactNode;
 }
-
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY ?? "", {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+  });
+}
 export default function Providers({ children }: Props) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      {children}
-    </ThemeProvider>
+    <PostHogProvider client={posthog}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </PostHogProvider>
   );
 }
